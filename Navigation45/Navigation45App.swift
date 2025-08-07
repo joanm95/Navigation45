c46556f82255d2c44e0a36c03878824e8b932787
Joan <joanm95@gmail.com>
2025-08-07 09:42:20 -0400

    Initial Commit

---
+++ Navigation45/Navigation45App.swift
@@ -0,0 +1,17 @@
+//
+//  Navigation45App.swift
+//  Navigation45
+//
+//  Created by Scholar on 8/7/25.
+//
+
+import SwiftUI
+
+@main
+struct Navigation45App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
